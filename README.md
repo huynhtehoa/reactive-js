6369bbd149db3454f8c9aa0e20dfc2b7fb7ce074
hoa huynh <huynhtehoa@gmail.com>
2024-08-02 12:41:55 +0700

    handle primitives

---
+++ README.md
@@ -0,0 +1,1 @@
+Simple reactivity system using [`Proxy`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Proxy)
--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-const targetMap = new Map();
+const targetMap = new WeakMap();
 
 function track(obj, prop, effect) {
+  if (!effect) return;
+
   const depMap = targetMap.get(obj) || new Map();
   const deps = depMap.get(prop) || new Set();
 
@@ -27,26 +29,38 @@ function watchEffect(cb) {
   activeEffect = null;
 }
 
-function reactive(obj) {
+function reactive(obj, { shallow } = { shallow: false }) {
   const proxiedObj = new Proxy(obj, {
     get(target, prop, reciever) {
-      if (activeEffect) {
-        if (typeof target[prop] === "object") {
-          target[prop] = reactive(obj[prop]);
-        }
-        track(target, prop, activeEffect);
+      if (!shallow && typeof target[prop] === "object") {
+        target[prop] = reactive(obj[prop]);
       }
+
+      track(target, prop, activeEffect);
       return Reflect.get(...arguments);
     },
     set(target, prop, newVal, reciever) {
       const res = Reflect.set(...arguments);
-      if (res) {
-        trigger(target, prop);
-      }
+      if (res) trigger(target, prop);
+
       return res;
     },
   });
   return proxiedObj;
+}
+
+function ref(raw) {
+  return {
+    _value: typeof raw === "object" ? reactive(raw) : raw,
+    get value() {
+      track(this, "_value", activeEffect);
+      return this._value;
+    },
+    set value(newVal) {
+      this._value = typeof newVal === "object" ? reactive(newVal) : newVal;
+      trigger(this, "_value");
+    },
+  };
 }
 
 (function runTest() {
@@ -55,13 +69,13 @@ function reactive(obj) {
 
   let target1;
   function test1() {
-    console.log('test1')
+    console.log("test1");
     target1 = reactiveObj1.a + 1;
   }
 
   let target2;
   function test2() {
-    console.log('test2')
+    console.log("test2");
     target2 = reactiveObj1.a + reactiveObj2.c.e;
   }
 
@@ -80,4 +94,19 @@ function reactive(obj) {
   reactiveObj1.a = 3;
   console.log(target1); // 4
   console.log(target2); // 10
+})();
+
+(function runTest2() {
+  let r1 = ref(1);
+  let r2 = ref(2);
+  let r3 = ref({ a: { b: 1 }, b: 2 });
+
+  let target;
+  watchEffect(() => (target = r1.value + r2.value + r3.value.a.b));
+
+  console.log(target); // 4
+  r1.value = 2;
+  console.log(target); // 5
+  r3.value.a.b = 2;
+  console.log(target); // 6
 })();
